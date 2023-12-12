94b8d3db3bbeeb3a6b1c5399f2f280f32cd43ceb
堀靖弘 <hori@clock-it.jp>
2023-12-12 13:57:37 +0900

    fix

--- pages/index.js
+++ pages/index.js
@@ -10,28 +10,16 @@ const Home = ({user, liff}) => {
       </Head>
 
       <main style={{height: "100%"}}>
-        <div style={{display: "flex", width: "100%", height: "50%"}}>
-          <div style={{backgroundColor: "red", width: "100%", height: "100%"}}>
-            <button onClick={() => window.open('', '_self').close()}>閉じる window.open('', '_self').close()</button>
-          </div>
-
-          <div style={{backgroundColor: "blue", width: "100%"}}>
-            <button onClick={() => liff.closeWindow()}>閉じる liff.closeWindow()</button>
-          </div>
-          <div onClick={() => location.href = '/detail'} style={{backgroundColor: "yellow", width: "100%"}}><span
-            style={{color: "red"}}>詳細</span>
-          </div>
+        <div style={{display: "flex", width: "100%", height: "100%"}}>
+          <a href="/detail" style={{backgroundColor: "red", width: "100%", height: "100%"}}>詳細</a>
+          <a href="/graph" style={{backgroundColor: "blue", width: "100%"}}>Highcharts</a>
+          <div style={{backgroundColor: "yellow", width: "100%"}}><span style={{color: "red"}}>userID：</span>{user?.userId?.substr(0, 10)}...</div>
         </div>
 
-
-        <div style={{backgroundColor: "blue", width: "100%"}}><span
-          style={{color: "red"}}>userID：</span>{user?.userId?.substr(0, 10)}...
-        </div>
-        <a href="/detail" style={{display: "flex", width: "100%", height: "50%"}}>
-          <div style={{backgroundColor: "yellow", width: "100%"}}><span
-            style={{color: "red"}}>userID：</span>{user?.userId?.substr(0, 10)}...
-          </div>
-          <div style={{backgroundColor: "red", width: "100%"}}><span>displayName：</span>{user?.displayName}</div>
+        <a href="/detail" style={{display: "flex", width: "100%", height: "100%"}}>
+          <div style={{backgroundColor: "blue", width: "100%"}}><span style={{color: "red"}}>userID：</span>{user?.userId?.substr(0, 10)}...</div>
+          <div style={{backgroundColor: "yellow", width: "100%"}}><span style={{color: "red"}}>userID：</span>{user?.userId?.substr(0, 10)}...</div>
+          <div style={{backgroundColor: "red", width: "100%"}}><span >displayName：</span>{user?.displayName}</div>
         </a>
       </main>
     </div>
